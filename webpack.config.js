86ee32fe2eef65c388379762571974f1734d09f3
Jason Werner <jasondwerner@gmail.com>
2016-10-20 17:06:55 +0300

    init commit. setup

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    path.join(__dirname, 'src/Root.jsx'),
+  ],
+  output: {
+    path: '/',
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  eslint: {
+    configFile: path.join(__dirname, '.eslintrc'),
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    preLoaders: [{
+      test: /\.jsx?$/,
+      loader: "eslint-loader",
+      exclude: /node_modules/
+    }],
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['', '.js', '.json', '.jsx'],
+  },
+};
