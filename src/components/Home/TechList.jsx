cdc4f0e3f3381428a7e1790e438981b87948c30d
TrillCyborg <jasondwerner@gmail.com>
2016-12-09 16:53:57 +0200

    make tech list dynamic

--- src/components/Home/TechList.jsx
+++ src/components/Home/TechList.jsx
@@ -3,62 +3,29 @@ import { List, ListItem } from 'material-ui/List';
 import Subheader from 'material-ui/Subheader';
 import Avatar from 'material-ui/Avatar';
 import Card from '../Card';
+import techList from './techList.const';
 
-const reactLogo = 'https://s3.amazonaws.com/media-p.slid.es/uploads/jhabdas/images/969312/react-logo-1000-transparent.png';
-const firebaseLogo = 'https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-128.png';
-const javascriptLogo = 'http://3.bp.blogspot.com/-PTty3CfTGnA/TpZOEjTQ_WI/AAAAAAAAAeo/KeKt_D5X2xo/s1600/js.jpg';
-const nodejsLogo = 'https://ih1.redbubble.net/image.109336634.1604/flat,550x550,075,f.u1.jpg';
-const reduxLogo = 'http://relinchos.com/assets/img/logo-tools/img-logo-redux.png';
-const mongoDBLogo = 'http://www.mongodb.sk/images/mongodb-leaf.png';
-const webRTCLogo = 'https://webrtc.org/assets/images/webrtc-logo-vert-retro-255x305.png';
-
-const TechList = () => (
-  <Card>
-    <List>
-      <Subheader>{"Tech I'm Using"}</Subheader>
+const TechList = () => {
+  function getList(type) {
+    return techList[type].map((item, i) => (
       <ListItem
+        key={i}
         disabled
-        leftAvatar={<Avatar src={javascriptLogo} style={{ backgroundColor: 'white' }} />}
-        primaryText="JavaScript"
+        leftAvatar={<Avatar src={item.logo} style={{ backgroundColor: 'white' }} />}
+        primaryText={item.name}
       />
-      <ListItem
-        disabled
-        leftAvatar={<Avatar src={reactLogo} style={{ backgroundColor: 'white' }} />}
-        primaryText="React"
-      />
-      <ListItem
-        disabled
-        leftAvatar={<Avatar src={reduxLogo} style={{ backgroundColor: 'white' }} />}
-        primaryText="Redux"
-      />
-      <ListItem
-        disabled
-        leftAvatar={<Avatar src={nodejsLogo} style={{ backgroundColor: 'white' }} />}
-        primaryText="Node.js"
-      />
-      <ListItem
-        disabled
-        leftAvatar={<Avatar src={mongoDBLogo} style={{ backgroundColor: 'white' }} />}
-        primaryText="MongoDB"
-      />
-      <Subheader>{"Tech I'm Learning"}</Subheader>
-      <ListItem
-        disabled
-        leftAvatar={<Avatar src={firebaseLogo} style={{ backgroundColor: 'white' }} />}
-        primaryText="Firebase"
-      />
-      <ListItem
-        disabled
-        leftAvatar={<Avatar src={reactLogo} style={{ backgroundColor: 'white' }} />}
-        primaryText="React Native"
-      />
-      <ListItem
-        disabled
-        leftAvatar={<Avatar src={webRTCLogo} style={{ backgroundColor: 'white' }} />}
-        primaryText="WebRTC"
-      />
-    </List>
-  </Card>
-);
+    ));
+  }
+  return (
+    <Card>
+      <List>
+        <Subheader>{"Tech I'm Using"}</Subheader>
+        {getList(0)}
+        <Subheader>{"Tech I'm Learning"}</Subheader>
+        {getList(1)}
+      </List>
+    </Card>
+  );
+};
 
 export default TechList;
---
+++ src/components/Home/techList.const.js
@@ -0,0 +1,41 @@
+const reactLogo = 'https://s3.amazonaws.com/media-p.slid.es/uploads/jhabdas/images/969312/react-logo-1000-transparent.png';
+const firebaseLogo = 'https://cdn4.iconfinder.com/data/icons/google-i-o-2016/512/google_firebase-2-128.png';
+const javascriptLogo = 'http://3.bp.blogspot.com/-PTty3CfTGnA/TpZOEjTQ_WI/AAAAAAAAAeo/KeKt_D5X2xo/s1600/js.jpg';
+const nodejsLogo = 'https://ih1.redbubble.net/image.109336634.1604/flat,550x550,075,f.u1.jpg';
+const reduxLogo = 'http://relinchos.com/assets/img/logo-tools/img-logo-redux.png';
+const mongoDBLogo = 'http://www.mongodb.sk/images/mongodb-leaf.png';
+const webRTCLogo = 'https://webrtc.org/assets/images/webrtc-logo-vert-retro-255x305.png';
+const techList = {
+  0: [// stuff i know
+    {
+      name: 'JavaScript',
+      logo: javascriptLogo,
+    }, {
+      name: 'React',
+      logo: reactLogo,
+    }, {
+      name: 'Redux',
+      logo: reduxLogo,
+    }, {
+      name: 'Node.js',
+      logo: nodejsLogo,
+    }, {
+      name: 'MongoDB',
+      logo: mongoDBLogo,
+    },
+  ],
+  1: [// stuff im learning
+    {
+      name: 'Firebase',
+      logo: firebaseLogo,
+    }, {
+      name: 'React Native',
+      logo: reactLogo,
+    }, {
+      name: 'WebRTC',
+      logo: webRTCLogo,
+    },
+  ],
+};
+
+export default techList;
